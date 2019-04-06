b4e583df67682293aedab29924749f8a1823dea0
Felipe de Almeida <32841875+fepirata@users.noreply.github.com>
2019-04-06 11:19:26 -0700

    Update Results.js

--- src/components/Results.js
+++ src/components/Results.js
@@ -6,12 +6,16 @@ const ResultList = props => {
         
         const dataResults = props.dataResult.map(dataResult => {
                 return <div className="dataResult">
-                                <p>{dataResult.name}</p>
+                <img src={dataResult.image_url} height="150"></img>
+                                <h2>{dataResult.name}</h2>
                                 <p>{!dataResult.is_closed ? 'Opened' : 'Closed'}</p>
                                 <p>{dataResult.price}</p>
                                 <p>{dataResult.review_count}</p>
                                 <p>{dataResult.rating}</p>
                                 <p>{dataResult.location.address1}</p>
+                                <p>{dataResult.phone}</p>
+                                <p>{dataResult.url}</p>
+                                
                         </div>
         });
         
